46c5c7c94124dcf0c694722576406e3751ad1a0c
Yijia <zeng.yi.jia@hotmail.com>
2024-03-14 16:05:27 +0100

    test doc2vec qui remplace tf-idf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from indexer import indexer
+from indexer_dcv import indexer  # Update import statement
 
 app = FastAPI()
 
@@ -14,12 +14,11 @@ async def index_documents(documents: DocumentList):
     return {"message": "Documents indexed successfully."}
 
 @app.get("/query-document")
-async def query_document(query: str, n: int = 5,threshold:float=0.5):
-    if indexer.document_matrix is None:
+async def query_document(query: str, n: int = 5, threshold: float = 0.1):
+    if indexer.doc2vec_model is None:  # Update this check if necessary
         raise HTTPException(status_code=404, detail="No indexed documents found.")
-    results = indexer.query_documents(query, n,threshold)
+    results = indexer.query_documents(query, n, threshold)
     return {"query": query, "results": results}
-
 
 @app.get("/retrieve-document/{index}")
 async def retrieve_document(index: int):
