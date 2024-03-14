86c3eb415ff62e41f5d0de4b0aa298401050e728
Yijia <zeng.yi.jia@hotmail.com>
2024-03-14 14:19:39 +0100

    add threshold of cosinus similarity score

--- indexer.py
+++ indexer.py
@@ -16,10 +16,14 @@ class DocumentIndexer:
             self.documents.extend(new_documents)
         self.document_matrix = self.tfidf_vectorizer.fit_transform(self.documents)
 
-    def query_documents(self, query, n=5):
+    def query_documents(self, query, n=5, threshold: float = 0.2):
         query_vec = self.tfidf_vectorizer.transform([query])
         cosine_similarities = linear_kernel(query_vec, self.document_matrix).flatten()
-        related_docs_indices = cosine_similarities.argsort()[:-n-1:-1]
+        
+        # Filtrer les documents basés sur le seuil de similarité cosine
+        related_docs_indices = [i for i, sim in enumerate(cosine_similarities) if sim >= threshold]
+        related_docs_indices = sorted(related_docs_indices, key=lambda i: cosine_similarities[i], reverse=True)[:n]
+
         return [(self.documents[i], cosine_similarities[i]) for i in related_docs_indices]
     def retrieve_document(self, index):
         if 0 <= index < len(self.documents):
--- main.py
+++ main.py
@@ -14,10 +14,10 @@ async def index_documents(documents: DocumentList):
     return {"message": "Documents indexed successfully."}
 
 @app.get("/query-document")
-async def query_document(query: str, n: int = 5):
+async def query_document(query: str, n: int = 5,threshold=0.2):
     if indexer.document_matrix is None:
         raise HTTPException(status_code=404, detail="No indexed documents found.")
-    results = indexer.query_documents(query, n)
+    results = indexer.query_documents(query, n,threshold)
     return {"query": query, "results": results}
 
 
